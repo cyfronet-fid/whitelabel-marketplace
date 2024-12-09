d8bedc7077149a85486b7a3bdf34c3392801aa16
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2024-12-09 10:04:52 +0100

    Add views, components for handle multiple objects

--- app/controllers/backoffice/statuses/providers_controller.rb
+++ app/controllers/backoffice/statuses/providers_controller.rb
@@ -1,23 +1,15 @@
 # frozen_string_literal: true
 
 class Backoffice::Statuses::ProvidersController < Backoffice::ProvidersController
-  before_action :action_type
-
   def create
     @providers = Provider.where(id: params[:provider_ids])
-    respond_to do |format|
-      if @providers.each { |p| "Provider::#{@action}".constantize.call(p) }
-        notice = "Selected providers was successfully #{@action}ed."
-        format.html { redirect_to providers_path(notice: notice) }
-        format.turbo_stream { flash.now[:notice] = notice }
-      else
-        format.html { render :index, status: :unprocessable_entity }
-        format.json { render :index, status: :unprocessable_entity }
-      end
+    @providers.each do |provider|
+      next if "Provider::#{params[:commit]}".constantize.call(provider)
+      redirect_to backoffice_statuses_providers_path,
+                  alert:
+                    "Provider #{provider.name} was not #{params[:commit]}ed. " +
+                      "Reason: #{provider.errors.full_messages.to_sentence}"
     end
-  end
-
-  def action_type
-    @action = params[:status_change] || "Publish"
+    redirect_to backoffice_statuses_providers_path, notice: "Selected Providers are successfully #{params[:commit]}ed."
   end
 end
