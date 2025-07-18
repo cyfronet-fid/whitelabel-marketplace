65a33bd067e8a045cf78539faf086ddf231459f9
Matthew Morris <matthew.morris@cessda.eu>
2025-07-18 10:27:00 +0200

    Fix Docker images not building if SECRET_KEY_BASE is `nil`

--- config/initializers/cookie_rotator.rb
+++ config/initializers/cookie_rotator.rb
@@ -4,8 +4,7 @@ Rails.application.config.after_initialize do
   Rails.application.config.action_dispatch.cookies_rotations.tap do |cookies|
     authenticated_encrypted_cookie_salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
     signed_cookie_salt = Rails.application.config.action_dispatch.signed_cookie_salt
-
-    secret_key_base = ENV.fetch("SECRET_KEY_BASE", Rails.application.credentials.secret_key_base)
+    secret_key_base = Rails.application.secret_key_base
 
     key_generator = ActiveSupport::KeyGenerator.new(
       secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
