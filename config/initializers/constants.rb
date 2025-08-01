54543f5b1997389af468d0428d1e1a23b0068b88
Pawel Gorczyca <goreck888@gmail.com>
2025-08-01 16:39:10 +0200

    fix: Fix previous user counter decrementation

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-MP_VERSION = ENV["MP_VERSION"] || File.read(Rails.root.join("VERSION")).strip
+MP_VERSION = ENV["MP_VERSION"] || File.read(Rails.root.join("version.txt")).strip
 
 # time after which user can rate service that starts when service become ready
 # default value is set to 90 days, ENV variable should represent number of days
