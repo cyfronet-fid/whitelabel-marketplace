1ed37662c105eec2979a701958128a719b52ccaa
jzbozien <j.zbozien@gmail.com>
2025-09-04 15:28:48 +0200

    style: [#197] provider profile styling 2

--- app/inputs/array_input.rb
+++ app/inputs/array_input.rb
@@ -17,7 +17,7 @@ class ArrayInput < SimpleForm::Inputs::TextInput
             :a,
             "Remove",
             id: "remove_#{object_name}_#{attribute_name}_#{index}",
-            class: "btn-special delete-button remove float-right disablable mt-2",
+            class: "btn-special clean delete-button remove float-right disablable mt-2",
             "data-target": attribute_name.to_s,
             "data-action": "click->form#removeArrayField",
             "data-value": "#{object_name}_#{attribute_name}_#{index}"
@@ -36,7 +36,7 @@ class ArrayInput < SimpleForm::Inputs::TextInput
                             :a,
                             "Remove",
                             id: "remove-#{object_name}_#{attribute_name}_#{number}",
-                            class: "btn-special delete-button remove float-right disablable mt-2",
+                            class: "btn-special clean delete-button remove float-right disablable mt-2",
                             "data-target": attribute_name.to_s,
                             "data-action": "click->form#removeArrayField",
                             "data-value": "#{object_name}_#{attribute_name}_#{number}"
