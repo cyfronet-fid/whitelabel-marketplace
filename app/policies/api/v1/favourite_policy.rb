cyfronet-fid/marketplace

feat:[#234] Enhance Favourites
- Extend Favourites to new entities
- Add a new view for Favourites
- Add favourites API

Closes #234

app/policies/api/v1/favourite_policy.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+class Api::V1::EssPolicy < ApplicationPolicy
+  def permitted_attributes
+    %i[pid type name links authors best_access_right]
+  end
+end

cyfronet-fid/whitelabel-marketplace

app/policies/api/v1/favourite_policy.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+class Api::V1::FavouritePolicy < ApplicationPolicy
+  def permitted_attributes
+    %i[pid type name links authors best_access_right]
+  end
+end
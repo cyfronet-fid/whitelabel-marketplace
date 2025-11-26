6674fddf94822730615fc6df1e047c1ec20f1d56
Pawel Gorczyca <goreck888@gmail.com>
2025-11-26 07:41:11 +0100

    feat:[#234] Enhance Favourites
    - Extend Favourites to new entities
    - Add a new view for Favourites
    - Add favourites API
    
    Closes #234

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_18_120000) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_18_104840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -960,6 +960,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_18_120000) do
     t.index ["user_id"], name: "index_user_categories_on_user_id"
   end
 
+  create_table "user_favourites", force: :cascade do |t|
+    t.bigint "favoritable_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "favoritable_type"
+    t.index ["favoritable_id", "user_id"], name: "index_user_favourites_on_favoritable_id_and_user_id", unique: true
+    t.index ["favoritable_id"], name: "index_user_favourites_on_favoritable_id"
+    t.index ["user_id"], name: "index_user_favourites_on_user_id"
+  end
+
   create_table "user_scientific_domains", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "scientific_domain_id"
@@ -968,16 +979,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_18_120000) do
     t.index ["scientific_domain_id"], name: "index_user_scientific_domains_on_scientific_domain_id"
     t.index ["user_id", "scientific_domain_id"], name: "index_usd_on_service_id_and_sd_id", unique: true
     t.index ["user_id"], name: "index_user_scientific_domains_on_user_id"
-  end
-
-  create_table "user_services", force: :cascade do |t|
-    t.bigint "service_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["service_id", "user_id"], name: "index_user_services_on_service_id_and_user_id", unique: true
-    t.index ["service_id"], name: "index_user_services_on_service_id"
-    t.index ["user_id"], name: "index_user_services_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -1078,8 +1079,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_18_120000) do
   add_foreign_key "tour_histories", "users"
   add_foreign_key "user_categories", "categories"
   add_foreign_key "user_categories", "users"
+  add_foreign_key "user_favourites", "services", column: "favoritable_id"
+  add_foreign_key "user_favourites", "users"
   add_foreign_key "user_scientific_domains", "scientific_domains"
   add_foreign_key "user_scientific_domains", "users"
-  add_foreign_key "user_services", "services"
-  add_foreign_key "user_services", "users"
 end
