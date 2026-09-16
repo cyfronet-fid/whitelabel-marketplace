1572933739e7905fb0ce5fe12738b81e53e5260d
Szymon Chodzidlo <szymon.chodzidlo@cyfronet.pl>
2026-09-16 14:50:02 +0200

    test: [#282] add specs for ImportAllJob

---
+++ spec/jobs/import_all_job_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ImportAllJob, backend: true do
+  let(:import_all_task) { instance_double(Rake::Task, invoke: true) }
+
+  before do
+    allow(Rails.application).to receive(:load_tasks)
+    allow(Rake::Task).to receive(:[]).with("import:all").and_return(import_all_task)
+  end
+
+  it "runs on the imports queue" do
+    expect(described_class.new.queue_name).to eq("imports")
+  end
+
+  context "when performed" do
+    before { described_class.perform_now }
+
+    it "loads the application's rake tasks" do
+      expect(Rails.application).to have_received(:load_tasks)
+    end
+
+    it "invokes the import:all rake task" do
+      expect(import_all_task).to have_received(:invoke)
+    end
+  end
+end
